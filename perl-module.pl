1a6b72ee78a25cc8e8ad0d908a38f1be1b9c0b05
Mark Grimes <mgrimes@cpan.org>
2010-09-07 12:19:00 +0000

    Minor tweaks
    git-svn-id: svn+ssh://vcs.grimes.homeip.net/var/lib/vcs/svn/mgrimes/dev/trunk/templates@541 aab9fa54-0a0a-0410-a3bb-be1775041f0f

--- perl-module.pl
+++ perl-module.pl
@@ -1,8 +1,7 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 
 use strict;
 use warnings;
-$|++;
 
 use Template;
 use File::Find;
@@ -10,14 +9,16 @@ use File::Path;
 use File::Basename;
 use File::Copy;
 use File::Spec;
-use FindBin;
+
+$|++;
 
 my $copy_re = qr/\.(png|gif|jpg|ico|pdf)$/;
-my $src = File::Spec->catdir( $FindBin::Bin, basename( $0, qw(.pl) ) );
+my $src = File::Spec->catpath( $ENV{HOME}, '.msorter' );
 
-my $namespace = shift || usage();
+my $namespace = shift || die "usage: $0 namespace\n";
 my $new_dir = lc $namespace;
 $new_dir =~ s/::/-/g;
+
 die "target dir ($new_dir) already exists\n" if -e $new_dir;
 print "creating new distribution directory: $new_dir\n";
 mkdir $new_dir or die "couldn't make new module dir ($new_dir): $!\n";
@@ -35,31 +36,31 @@ use Term::ReadLine;
 my $term = new Term::ReadLine 'Input> ';
 
 sub new {
-	my $that  = shift;
-	my $class = ref($that) || $that;    # Enables use to call $instance->new()
-	my $self = shift || {};
-	bless $self, $class;
-	return $self;
+    my $that  = shift;
+    my $class = ref($that) || $that;    # Enables use to call $instance->new()
+    my $self  = shift || {};
+    bless $self, $class;
+    return $self;
 }
 
 sub AUTOLOAD {
-	my $self = shift;
-	my $type = ref($self) or die "$self is not an object";
+    my $self = shift;
+    my $type = ref($self) or die "$self is not an object";
 
-	my $name = $AUTOLOAD;
-	$name =~ s/.*://;           # Strip fully qualified portion
+    my $name = $AUTOLOAD;
+    $name =~ s/.*://;                   # Strip fully qualified portion
 
-	# ensure none of these are DESTROY
-	return if $name =~ /^DESTROY$/; 
+    # ensure none of these are DESTROY
+    return if $name =~ /^DESTROY$/;
 
-	# print "AUTOLOAD ($name)\n";
+    # print "AUTOLOAD ($name)\n";
 
-	# Return the value of the field, setting if an argument supplied
-	return $self->{$name} if exists $self->{$name};
+    # Return the value of the field, setting if an argument supplied
+    return $self->{$name} if exists $self->{$name};
 
-	my $v = $term->readline("Enter $name: ");
-	$self->{$name} = $v;
-	return $v;
+    my $v = $term->readline("Enter $name: ");
+    $self->{$name} = $v;
+    return $v;
 }
 
 1;
@@ -67,67 +68,59 @@ sub AUTOLOAD {
 package main;
 
 my $hash = My::Template::ReadLine->new( {
-		name	=> 'Mark V. Grimes',
-		email	=> 'mgrimes@cpan.org',
-		year	=> $time[5] + 1900,
-		namespace => $namespace,
-		pm_filename => $pm_filename,
-	} );
+        name        => 'Mark Grimes',
+        email       => 'mgrimes@cpan.org',
+        year        => $time[5] + 1900,
+        namespace   => $namespace,
+        pm_filename => $pm_filename,
+} );
 my $vars = { v => $hash };
 
-my $tt = Template->new( {
-		INCLUDE_PATH	=> $src,
-	});
+my $tt = Template->new( { INCLUDE_PATH => $src, } );
 
 find( { wanted => \&wanted, no_chdir => 1 }, $src );
 
 print <<CVS_DOC;
 cd $new_dir
-svn import svn+ssh://grimes.homeip.net/var/svn/mgrimes/dev/trunk/$new_dir
-cd .. && mv $new_dir $new_dir.org
-svn co svn+ssh://grimes.homeip.net/var/svn/mgrimes/dev/trunk/$new_dir
+git init .
+git add .
+git commit -a
+git-2-git add . 
 CVS_DOC
 
-sub wanted { 
-	my $f = $File::Find::name;
+sub wanted {
+    my $f = $File::Find::name;
 
-	return if $f eq $src;		# don't create the src dir
-	my $n = $f; $n =~ s!$src/!!;
-	my $d = "$new_dir/$n";
-	
-	# print "* $f ->\n\tn= $n\n\td= $d\n";
+    return if $f eq $src;    # don't create the src dir
+    my $n = $f;
+    $n =~ s!$src/!!;
+    my $d = "$new_dir/$n";
 
-	if( -d $f ){
-		if( m!/CVS$|/.svn$! ){	# don't create the CVS or svn dirs
-			$File::Find::prune = 1;
-			return;
-		}
-		print "creating new dir: $d = $_\n";
-		mkpath $d or die "couldn't create directory ($d): $!\n";
-		return;
-	}
+    # print "* $f ->\n\tn= $n\n\td= $d\n";
 
-	if( $f =~ $copy_re ){
-		print "copying file: $d\n";
-		copy($f,$d) or die "error copying $f to $d: $!\n";
-		return;
-	}
+    if ( -d $f ) {
+        if (m!/CVS$|/.svn$!) {    # don't create the CVS or svn dirs
+            $File::Find::prune = 1;
+            return;
+        }
+        print "creating new dir: $d = $_\n";
+        mkpath $d or die "couldn't create directory ($d): $!\n";
+        return;
+    }
 
-	if( $f =~ /NAMESPACE/ ){
-		my $ns = $namespace;
-		$ns =~ s!::!/!g;
-		$d  =~ s!NAMESPACE!$ns!;
-	}
+    if ( $f =~ $copy_re ) {
+        print "copying file: $d\n";
+        copy( $f, $d ) or die "error copying $f to $d: $!\n";
+        return;
+    }
 
-	print "processing $d\n";
-	$tt->process($n, $vars, $d) || die $tt->error();
+    if ( $f =~ /NAMESPACE/ ) {
+        my $ns = $namespace;
+        $ns =~ s!::!/!g;
+        $d  =~ s!NAMESPACE!$ns!;
+    }
+
+    print "processing $d\n";
+    $tt->process( $n, $vars, $d ) || die $tt->error();
 }
 
-sub usage {
-
-print <<END;
-usage: $0 namespace
-END
-
-exit;
-}
